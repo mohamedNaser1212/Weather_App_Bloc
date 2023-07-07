8e409248bea29048413947059a7e635496b54cbb
mohamedNaser1212 <nasergamal33e@gmail.com>
2023-07-07 17:55:16 +0300

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
+import 'package:weather_app/cubits/weather_cubit.dart';
+import 'package:weather_app/main.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:weather_app/pages/search_page.dart';
+import 'package:weather_app/providers/weather_provider.dart';
+import 'package:weather_app/services/weather_service.dart';
+
+import '../cubits/weather_state.dart';
+import '../models/weather_model.dart';
+import '../models/weather_model.dart';
+import '../models/weather_model.dart';
+
+
+class HomePage extends StatelessWidget  {
+
+
+  WeatherModel? weatherData;
+  @override
+  Widget build(BuildContext context) {
+
+
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return SearchPage(
+
+                );
+              }));
+            },
+            icon: Icon(Icons.search),
+          ),
+        ],
+        title: Text('Weather App'),
+      ),
+      body:BlocBuilder<WeatherCubit,WeatherState>(
+        builder: (context,state){
+          if(state is WeatherLoading){
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+
+          }else if(state is WeatherSuccess){
+
+            return SuccessBody(weatherData: state.weatherModel);
+          }else if(state is WeatherFailure){
+            return const Center(
+              child:Text('Something went wrong'),
+            );
+          }else {
+            return Defaultbody();
+          }
+
+        },
+      )
+    );
+  }
+}
+
+class Defaultbody extends StatelessWidget {
+  const Defaultbody({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text('there is no weather Start  ',style: TextStyle(
+            fontSize: 30,
+          ),),
+          Text('Searching Now ',style: TextStyle(
+            fontSize: 30
+          ),),
+        ],
+
+      ),
+    );
+  }
+}
+class SuccessBody extends StatelessWidget {
+  const SuccessBody({required this.weatherData ,Key? key}) : super(key: key);
+final WeatherModel? weatherData;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              weatherData!.getThemeColor(),
+              weatherData!.getThemeColor()[300]!,
+              weatherData!.getThemeColor()[100]!,
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          )),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Spacer(
+            flex: 3,
+          ),
+          Text(
+            BlocProvider.of<WeatherCubit>(context).cityName!,
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            'updated at : ${weatherData!.date.hour.toString()}:${weatherData!.date.minute.toString()}',
+            style: TextStyle(
+              fontSize: 22,
+            ),
+          ),
+          Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image.asset(weatherData!.getImage()),
+              Text(
+                weatherData!.temp.toInt().toString(),
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Column(
+                children: [
+                  Text('maxTemp :${weatherData!.maxTemp.toInt()}'),
+                  Text('minTemp : ${weatherData!.minTemp.toInt()}'),
+                ],
+              ),
+            ],
+          ),
+          Spacer(),
+          Text(
+            weatherData!.weatherStateName,
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Spacer(
+            flex: 5,
+          ),
+        ],
+      ),
+    );
+  }
+}
